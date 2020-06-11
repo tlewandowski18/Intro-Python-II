2bf9682bfff1fab4022a5909d5b97ade52eaf76c
tlewandowski18 <tlewandowski18@gmail.com>
2020-06-11 17:41:15 -0400

    Add game functionality

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,37 @@
 from room import Room
 from player import Player
 
+import re
+
+
+
+
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     """North of you, the cave mount beckons
+                     """),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.
+"""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.
+"""),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.
+"""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.
+"""),
 }
+
 
 
 # Link rooms together
@@ -41,6 +52,41 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+
+player = Player('Tom', room['outside'])
+
+print(f'\nYour quest begins at the {player.current_room.name}\n')
+print(f'{player.current_room.description}\n')
+
+
+while True:
+    choice = input(f"Please choose a direction (n, s, e, w) or enter 'q' to quit: ")
+    try:
+        if choice == "q":
+            print('Your quest has ended')
+            break
+        elif choice == "n":
+            player.current_room = player.current_room.n_to
+            print(f'\n{player.current_room.name}\n')
+            print(player.current_room.description)
+        elif choice == "s":
+            player.current_room = player.current_room.s_to
+            print(f'\n{player.current_room.name}\n')
+            print(player.current_room.description)
+        elif choice == "e":
+            player.current_room = player.current_room.e_to
+            print(f'\n{player.current_room.name}\n')
+            print(player.current_room.description)
+        elif choice == "w":
+            player.current_room = player.current_room.w_to
+            print(f'\n{player.current_room.name}\n')
+            print(player.current_room.description)
+        else:
+            raise ValueError
+    except ValueError:
+        print("Please enter a valid direction")
+    except AttributeError:
+        print(f"\nThere is no room in that direction. Please try again.")
 
 # Write a loop that:
 #
--- src/player.py
+++ src/player.py
@@ -4,3 +4,6 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+
+    def __str__(self):
+        return f'{self.name} is currently in the {self.current_room.name}'
